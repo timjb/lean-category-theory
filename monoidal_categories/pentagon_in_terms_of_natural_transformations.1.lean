fdb4786e3eb188b35295889bab9ed64d1d7334a2
Scott Morrison <scott.morrison@anu.edu.au>
2017-03-04 13:24:09 +1100

    moving the new working proof of the tranformation pentagon onto the no-extends branch

--- monoidal_categories/pentagon_in_terms_of_natural_transformations.1.lean
+++ monoidal_categories/pentagon_in_terms_of_natural_transformations.1.lean
@@ -16,11 +16,16 @@ namespace tqft.categories.monoidal_category
 lemma pentagon_in_terms_of_natural_transformations
   ( C : MonoidalCategory ) : pentagon_2step C = pentagon_3step C :=
   begin
-    -- unfold pentagon_2step,
-    -- unfold pentagon_3step,
-    blast,
-    -- apply C^.pentagon
-    exact sorry
+    blast, -- This just unfolds definitions and introduces variables
+    induction X with PQR S,
+    induction PQR with PQ R,
+    induction PQ with P Q,
+    pose p := C^.pentagon P Q R S,
+    blast, -- this simplifies the hypothesis p back to something reasonable
+    repeat { rewrite Functor.identities C^.tensor }, -- we shouldn't have to do this, as Functor.identities has [simp]
+    blast,                                           -- cleaning up mess...
+    repeat { rewrite C^.right_identity },            -- again, we shouldn't need to do these, Category.left_identity has [simp] too
+    repeat { rewrite C^.left_identity }
   end
 
 end tqft.categories.monoidal_category
